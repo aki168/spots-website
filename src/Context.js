446f360dafdfae5bbc41dd343ba77095a1a8a5a7
BECREATOR/aki.zheng <ahsi.aki@gmail.com>
2022-12-14 11:01:54 +0800

    Updates

--- src/Context.js
+++
@@ -1,7 +0,0 @@
-import { createContext, useContext } from 'react'
-
-export const AuthContext = createContext(null) // for App.js
-
-export const useAuth = () => { // for dece
-    return useContext(AuthContext)
-}
